65c1d7a415e1bf3b4060ff29f6a350e28f2af8ae
neoz666 <neoz.blockchain@gmail.com>
2023-12-19 09:24:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+// import { NFTStorage, Blob } from 'nft.storage';
+import { NFTStorage, File, Blob } from 'https://cdn.skypack.dev/nft.storage';
+
+
+const fr = new FileReader();
+const client = new NFTStorage({ token: process.env.NFT_STORAGE_TOKEN });
+
+async function uploadFile(file) {
+    fr.readAsArrayBuffer(file);
+    fr.onloadend = async () => {
+        const fileBlob = new Blob([fr.result]);
+        const fileCid = await client.storeBlob(fileBlob);
+        console.log({ fileCid });
+        const metadata = await client.store({
+            metadata: {
+                file: fileCid,
+            },
+        });
+        console.log({ metadata });
+        const data = new Blob([JSON.stringify(metadata)]);
+        const { CAR } = await NFTStorage.encodeBlob(data);
+        const cid = await client.storeCAR(CAR);
+        console.log({ cid });
+    };
+}
+
+window.handleUpload = function handleUpload() {
+    const fileInput = document.getElementById('fileInput');
+    const inputFile = fileInput.files[0];
+
+    if (inputFile) {
+        uploadFile(inputFile);
+    } else {
+        console.error('No file selected.');
+    }
+}
