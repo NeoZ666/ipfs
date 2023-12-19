f6ed8791679cb9a00bc74d0490366d2f60b07d5f
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-12-19 22:15:06 +0530

    React-app os nft-storage IPFS(non-functional)

---
+++ nft-storage-ipfs/src/App.js
@@ -0,0 +1,130 @@
+// import React, { useState } from "react";
+// // import { create } from "ipfs-http-client"; // Importing the specific functionality needed
+// import { NFTStorage, File, Blob } from "https://cdn.skypack.dev/nft.storage";
+
+// const App = () => {
+//   // const [audioFile, setAudioFile] = useState(null);
+//   // const [cid, setCID] = useState("");
+
+//   // const ipfs = create({
+//   //   host: "ipfs.infura.io",
+//   //   port: 5001,
+//   //   protocol: "https",
+//   // });
+
+//   // const handleFileChange = (event) => {
+//   //   setAudioFile(event.target.files[0]);
+//   // };
+
+//   // const uploadToIPFS = async () => {
+//   //   if (!audioFile) {
+//   //     alert("Please select an audio file.");
+//   //     return;
+//   //   }
+
+//   //   try {
+//   //     const fileAdded = await ipfs.add(audioFile);
+//   //     setCID(fileAdded.cid.toString());
+//   //   } catch (error) {
+//   //     console.error("Error uploading to IPFS:", error);
+//   //   }
+//   // };
+
+//   const fr = new FileReader();
+//   const client = new NFTStorage({ token: process.env.NFT_STORAGE_TOKEN });
+
+//   async function uploadFile(file) {
+//     fr.readAsArrayBuffer(file);
+//     fr.onloadend = async () => {
+//       const fileBlob = new Blob([fr.result]);
+//       const fileCid = await client.storeBlob(fileBlob);
+//       console.log({ fileCid });
+//       const metadata = await client.store({
+//         metadata: {
+//           file: fileCid,
+//         },
+//       });
+//       console.log({ metadata });
+//       const data = new Blob([JSON.stringify(metadata)]);
+//       const { CAR } = await NFTStorage.encodeBlob(data);
+//       const cid = await client.storeCAR(CAR);
+//       console.log({ cid });
+//     };
+//   }
+
+//   window.handleUpload = function handleUpload() {
+//     const fileInput = document.getElementById("fileInput");
+//     const inputFile = fileInput.files[0];
+
+//     if (inputFile) {
+//       uploadFile(inputFile);
+//     } else {
+//       console.error("No file selected.");
+//     }
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>File Upload</h1>
+//       <input type="file" id="fileInput" />
+//       <button onclick="handleUpload()">Upload</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { useState } from "react";
+import { NFTStorage, Blob } from "https://cdn.skypack.dev/nft.storage";
+
+const App = () => {
+  const [cid, setCID] = useState("");
+
+  const client = new NFTStorage({ token: process.env.NFT_STORAGE_TOKEN });
+
+  async function uploadFile(file) {
+    const fr = new FileReader();
+    fr.readAsArrayBuffer(file);
+
+    fr.onloadend = async () => {
+      const fileBlob = new Blob([fr.result]);
+      const fileCid = await client.storeBlob(fileBlob);
+      console.log({ fileCid });
+
+      const metadata = await client.store({
+        metadata: {
+          file: fileCid,
+        },
+      });
+      console.log({ metadata });
+
+      const data = new Blob([JSON.stringify(metadata)]);
+      const { CAR } = await NFTStorage.encodeBlob(data);
+      const cid = await client.storeCAR(CAR);
+      console.log({ cid });
+      setCID(cid); // Update the state with the CID
+    };
+  }
+
+  const handleUpload = () => {
+    const fileInput = document.getElementById("fileInput");
+    const inputFile = fileInput.files[0];
+
+    if (inputFile) {
+      uploadFile(inputFile);
+    } else {
+      console.error("No file selected.");
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>File Upload</h1>
+      <input type="file" id="fileInput" />
+      <button onClick={handleUpload}>Upload</button>
+      {cid && <p>CID: {cid}</p>}
+    </div>
+  );
+};
+
+export default App;
